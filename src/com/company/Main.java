f86d76b224f09f6dbbee137e27aa80f9a4631e78
hussainamran <hussainamran1978@gmail.com>
2022-01-29 11:53:28 -0500

    Feature sun witherror needs to resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,11 @@ public class Main {
         milkway.sun=1;
         milkway.moon=2;
         milkway.planets=3;
+
+        Sun sloana=new Sun();
+        sloana.heat="83485y843y5";
+        sloana.radius="y5ye5eyue";
+        sloana.temp=123232144a;
+
     }
 }
