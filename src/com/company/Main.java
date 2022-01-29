511aafc194bb4cb961c4853c91c95d0ae97a7e84
hussainamran <hussainamran1978@gmail.com>
2022-01-29 11:04:24 -0500

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         solorSystem milkway=new solorSystem();
+        milkway.sun=1;
+        milkway.moon=2;
+        milkway.planets=3;
     }
 }
